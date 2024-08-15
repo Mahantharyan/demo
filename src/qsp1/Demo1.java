1b3cd571bc81959838d36bed6f66537ee5d1fc01
mr <mr@DESKTOP-9SFDE2T>
2024-08-15 01:54:46 -0700

    star pattern

---
+++ src/qsp1/Demo1.java
@@ -0,0 +1,27 @@
+package qsp1;
+
+import org.testng.annotations.Test;
+
+public class Demo1
+{
+@Test
+public void Appln2()
+{
+	for(int i=1;i<=3;i++)
+		{
+			for(int j=1;j<=i;j++)
+			{
+				System.out.print("*");
+			}
+			System.out.println();
+		}
+		for(int i =2;i>=1;i--)
+		{
+			for(int j=1;j<=i;j++) 
+			{
+				System.out.print("*");
+				
+			}
+			System.out.println();
+		}}}
+
